16f36f2f90077453d30b81b20a5e11370144d5a6
RJ Smith <rj@rjsmith.me>
2020-01-03 20:53:24 -0500

    Cleaned up some of the formatting.

--- discordBot.py
+++ discordBot.py
@@ -27,7 +27,8 @@ async def on_ready():
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hello {member.name}, welcome to Best of Us.  Be sure to check out our server info and welcome mat channels.  If you need any assistance with roles or have questions at all, let one of the Admin staff know. - Alch'
+        f'Hello {member.name}, welcome to Best of Us.  Be sure to check out our server info and welcome mat channels.  '
+        f'If you need any assistance with roles or have questions at all, let one of the Admin staff know. - Alch'
     )
 
 # Commands Section - All bot commands will be included here.
@@ -35,16 +36,17 @@ async def on_member_join(member):
 # Tells the user the current time in EVE Online based on ~time command.
 @bot.command(name='time', help='This command displays the current EVE Online time.')
 async def eve_time(ctx):
-    tz_UTC = pytz.timezone('UTC')
-    datetime_UTC = datetime.now(tz_UTC)
+    tz_utc = pytz.timezone('UTC')
+    datetime_utc = datetime.now(tz_utc)
     get_eve_time = datetime_UTC.strftime("%H:%M:%S")
 
     await ctx.send(get_eve_time)
 
 # Gives the user a link to the current BAH Calculator.
-@bot.command(name='bah', help='This command will post a link to the current Basic Allowance for Housing (BAH) calculator.')
+@bot.command(name='bah', help='This command will post a link to the current Basic Allowance for Housing (BAH) '
+                              'calculator.')
 async def bah_calc(ctx):
-    give_bah = ('https://www.defensetravel.dod.mil/site/bahCalc.cfm')
+    give_bah = 'https://www.defensetravel.dod.mil/site/bahCalc.cfm'
 
     await ctx.send(give_bah)
 
